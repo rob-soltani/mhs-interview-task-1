8b99ba93e6f6aa92602a1166ce32b33156ee9615
Rob Soltani - Sohrab-Dell <rob@soltani.io>
2022-08-09 22:57:36 -0400

    Shortcode Registration Test
    Added an initial Generate method (callback) for the shortcode in src/Plugin.php. Updated the minimum WordPress version from 5.3 to 6.0.1. Also, updated the minimum PHP version from 5.6 to 7.4.26.

--- plugin.php
+++ plugin.php
@@ -5,8 +5,8 @@
  * Plugin URI: https://rob.soltani.io/
  * Description: A plugin that adds a shortcode to display a notice box based on specified type and message.
  * Version: 0.0.1
- * Requires at least: 5.3
- * Requires PHP: 5.6
+ * Requires at least: 6.0.1
+ * Requires PHP: 7.4.26
  * Author: Rob Soltani
  * Author URI: https://rob.soltani.io
  * License: GPL v2 or later
--- src/Plugin.php
+++ src/Plugin.php
@@ -7,11 +7,24 @@ namespace MHS_INTERVIEW_TASK_1;
 class NoticeBox
 {
 
+    // A function responsible for generating the HTML code for the shortcode
+    public static function Generate($Type, $Title, $Message)
+    {
+
+        $HTML  = '<div class="">';
+        $HTML .= 'Testing MHS Interview Task 1\'s shortcode ...';
+        $HTML .= '</div>';
+
+        return $HTML;
+        
+    }
+
+    // A function to initialize and register the NoticeBox shortcode
     public static function Initiate()
     {
 
-        
-        
+        // Register the shortcode using plugin-specific tag and it's callback
+        add_shortcode( 'MHS_INTERVIEW_TASK_1_NoticeBox', 'MHS_INTERVIEW_TASK_1\NoticeBox::Generate' );
         
     }
 
