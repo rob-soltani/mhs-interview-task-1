5aeff0ab6992991fe5174adbff1032d5bb56e4be
Rob Soltani - Sohrab-Dell <rob@soltani.io>
2022-08-09 22:17:32 -0400

    Initial Template
    plugin.php created with plugin header and namespace.

---
+++ plugin.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Plugin Name: MHS Interview Task 1
+ * Plugin URI: https://rob.soltani.io/
+ * Description: A plugin that adds a shortcode to display a notice box based on specified type and message.
+ * Version: 0.0.1
+ * Requires at least: 5.3
+ * Requires PHP: 5.6
+ * Author: Rob Soltani
+ * Author URI: https://rob.soltani.io
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: mhs_interview_task_1
+ * Domain Path: /public/lang
+ */
+
+namespace MHS_INTERVIEW_TASK_1;
+
+
